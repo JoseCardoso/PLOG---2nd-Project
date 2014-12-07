8403611cf6e29e37cf86064d9a7a39ae5267c857
Jose Cardoso <josecardos@gmail.com>
2014-12-07 19:55:17 +0000

    Started Coding

--- src/src.pl
+++ src/src.pl
@@ -1,7 +1,34 @@
 :-use_module(library(clpfd)).
 
 
+func(Horario,Salario):- Salario  #<= 7*80, Horario #<= 7.
 
+horario_func([0,0,0,0,0,0,0,0]).
+
+adic_func_parcial.
+
+adic_func_normal.
+
+adic_func_normal_extra.
+
+adic_func_parcil_extra.
+
+
+dist_func:-
+        Extras  #<= 2,
+        Dinheiro,
+        Vars=[H1,H2,H3,H4,H5,H6,H7,H8],
+        H1 #> 3,
+        H2 #> 2,
+        H3 #> 2,
+        H4 #> 5,
+        H5 #> 4,
+        H6 #> 5,
+        H7 #> 7,
+        H8 #> 7,
+        Parciais #< 6,
+        Parciais/(Parciais+Normais) #<= 0.3,
+        
 
 
 
